a53be14a867e6eb25192b61b7f6a5bc17bbc8176
tornvall <tornvall@stud.ntnu.no>
2016-01-22 16:20:06 +0100

    chopper is moving yo

--- app/src/main/java/com/example/fredrik/mygame/TitleScreen.java
+++ app/src/main/java/com/example/fredrik/mygame/TitleScreen.java
@@ -1,6 +1,7 @@
 package com.example.fredrik.mygame;
 
 
+
 import sheep.game.Sprite;
 import sheep.game.State;
 import sheep.graphics.Image;
@@ -9,26 +10,29 @@ import sheep.graphics.Image;
  * Created by Fredrik on 21/01/16.
  */
 public class TitleScreen extends State {
-    private Image heliImage = new Image(R.drawable.heli1_east);
-    private Image aImage = new Image(R.drawable.icon);
+
+    private Image aImage = new Image(R.drawable.heli1_east);
     private Image wallVerImage = new Image(R.drawable.wall_vertical);
+    private Image wallVerImage2 = new Image(R.drawable.wall_vertical);
     private Image backgroundImage = new Image(R.drawable.background);
     private Sprite aSprite;
     private Sprite westWall;
+    private Sprite eastWall;
     private Sprite backSprite;
-    private Sprite heliSprite;
 
 
     public TitleScreen() {
         backSprite = new Sprite(backgroundImage);
-        heliSprite = new Sprite(heliImage);
         aSprite = new Sprite(aImage);
+
         westWall = new Sprite(wallVerImage);
-        westWall.setPosition(4, 215);
+        westWall.setPosition(4, 220);
+
+        eastWall = new Sprite(wallVerImage2);
+        eastWall.setPosition(476, 220);
+
         aSprite.setPosition(200, 120);
-        aSprite.setSpeed(40, 0); // it should move right direction, but since collides bug, it will move (-40,0),  If we input (-40,0), it move (40,0), after collides, helicopter is disappeared. bug?
-        heliSprite.setPosition(40, 120);
-        heliSprite.setSpeed(40, 0);
+        aSprite.setSpeed(-40, 0); // it should move right direction, but since collides bug, it will move (-40,0),  If we input (-40,0), it move (40,0), after collides, helicopter is disappeared. bug?
 
     }
 
@@ -36,42 +40,29 @@ public class TitleScreen extends State {
         backSprite.draw(canvas);
 
         westWall.draw(canvas);
+        eastWall.draw(canvas);
         aSprite.draw(canvas);
-        heliSprite.draw(canvas);
     }
 
     public void update(float dt) {
 
-        if(aSprite.getX()>=300)
+        if(aSprite.collides(eastWall))
         {
             System.out.println("crash east border!");
             aSprite.setSpeed(-aSprite.getSpeed().getX(), aSprite.getSpeed().getY());
+            aSprite.setScale(-1, 1);
         }
 
         else if(aSprite.collides(westWall)) // collides is true first time, and change the object direction.
         {
             System.out.println("crash west border!");
             aSprite.setSpeed(-aSprite.getSpeed().getX(), aSprite.getSpeed().getY());
-        }
-
-        else if(aSprite.collides(heliSprite)) // This collides is judged since the above collides. First execution collides will be true at the first time without any judge.
-        {
-            System.out.println("crash each other!");
-            aSprite.setSpeed(-aSprite.getSpeed().getX(), aSprite.getSpeed().getY());
-            heliSprite.setScale(-1, 1);
-            heliSprite.setPosition(heliSprite.getPosition().getX() + heliImage.getWidth(), heliSprite.getPosition().getY());
-            heliSprite.setSpeed(-aSprite.getSpeed().getX(), aSprite.getSpeed().getY());
-        }
-
-        if(heliSprite.getX()>=300)
-        {
-            System.out.println("crash east border!");
-            heliSprite.setSpeed(-heliSprite.getSpeed().getX(), heliSprite.getSpeed().getY());
+            aSprite.setScale(1, 1);
         }
 
         westWall.update(dt);
+        eastWall.update(dt);
         aSprite.update(dt);
-        heliSprite.update(dt);
 
     }
 
