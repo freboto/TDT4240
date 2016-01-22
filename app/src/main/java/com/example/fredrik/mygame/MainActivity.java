1440b2213c44805f1e7c0660d4255375b4e0259a
tornvall <tornvall@stud.ntnu.no>
2016-01-22 14:20:39 +0100

    first commit

---
+++ app/src/main/java/com/example/fredrik/mygame/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.fredrik.mygame;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import sheep.game.Game;
+
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        // Create the game.
+        Game game = new Game(this, null);
+
+        // Push the main state.
+        game.pushState(new TitleScreen());
+
+        // View the game.
+        setContentView(game);
+        }
+}
